98358d835ecfbe28ecb3b5a11564e25675a39c94
psbworld6 <133992142+psbworld6@users.noreply.github.com>
2023-06-09 12:46:08 +0530

    Update copy_sheet_from_excel.py

--- copy_sheet_from_excel.py
+++ copy_sheet_from_excel.py
@@ -1,14 +1,16 @@
 import os
 from openpyxl import load_workbook
 from openpyxl import Workbook
-from openpyxl.styles import Font, PatternFill, Border, Alignment
-from openpyxl.utils import get_column_letter
 
 # Get the directory path where the Excel files are located
 directory = '/path/to/excel/files'  # Replace with the actual directory path
 
 # Create a new workbook to store the consolidated sheets
 consolidated_workbook = Workbook()
+
+# Create the destination sheet in the consolidated workbook
+consolidated_sheet = consolidated_workbook.active
+consolidated_sheet.title = 'Consolidated Test Report'
 
 # Iterate over each file in the directory
 for filename in os.listdir(directory):
@@ -22,37 +24,14 @@ for filename in os.listdir(directory):
             # Get the reference to the source sheet
             source_sheet = workbook["Consolidated Test File"]
 
-            # Create a new sheet in the consolidated workbook
-            new_sheet = consolidated_workbook.create_sheet(title=filename)
+            # Copy the sheet with formatting and alignment
+            for row in source_sheet.iter_rows(values_only=True):
+                consolidated_sheet.append(row)
 
-            # Copy the sheet with formatting and content
-            for row in source_sheet.iter_rows(min_row=1, max_row=source_sheet.max_row, min_col=1, max_col=source_sheet.max_column):
-                for cell in row:
-                    new_cell = new_sheet[cell.coordinate]
-                    new_cell.data_type = cell.data_type
-                    new_cell.value = cell.value
+            # Rename the sheet to the file name
+            consolidated_sheet.title = filename
 
-                    # Copy the cell's style
-                    if cell.has_style:
-                        new_cell.font = Font.from_tuple(cell.font)
-                        new_cell.fill = PatternFill.from_tuple(cell.fill)
-                        new_cell.border = Border.from_tuple(cell.border)
-                        new_cell.number_format = cell.number_format
-
-                    # Copy the cell's alignment
-                    if cell.alignment:
-                        new_cell.alignment = Alignment(
-                            horizontal=cell.alignment.horizontal,
-                            vertical=cell.alignment.vertical,
-                            wrap_text=cell.alignment.wrap_text,
-                            shrink_to_fit=cell.alignment.shrink_to_fit,
-                            indent=cell.alignment.indent,
-                        )
-
-            # Rename the new sheet to the file name
-            new_sheet.title = filename
-
-print("Copying, formatting, and renaming sheets completed.")
+print("Copying and renaming sheets completed.")
 
 # Save the consolidated workbook to a new file
 consolidated_file_path = os.path.join(directory, "consolidated_reports.xlsx")
