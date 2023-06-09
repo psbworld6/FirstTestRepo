c92bb128b4dd276fd339ea4d611d3b5ca3fb4509
psbworld6 <133992142+psbworld6@users.noreply.github.com>
2023-06-09 13:19:15 +0530

    Update copy_sheet_from_excel.py

--- copy_sheet_from_excel.py
+++ copy_sheet_from_excel.py
@@ -16,18 +16,39 @@ for filename in os.listdir(directory_path):
         try:
             date_obj = datetime.strptime(month_year, '%B%Y')
             month_year_formatted = date_obj.strftime('%B%Y')
-            
+
             # Open the source workbook and select the desired sheet
             source_workbook = openpyxl.load_workbook(os.path.join(directory_path, filename))
             source_sheet = source_workbook["Consolidated Test Report"]
-            
+
             # Create a new sheet in the consolidated workbook
             new_sheet = consolidated_workbook.create_sheet(title=month_year_formatted)
-            
-            # Copy the sheet contents
+
+            # Copy the sheet contents along with formatting
             for row in source_sheet.iter_rows(values_only=True):
                 new_sheet.append(row)
-            
+
+            # Copy the sheet formatting
+            for row in source_sheet.iter_rows():
+                for cell in row:
+                    new_cell = new_sheet.cell(row=cell.row, column=cell.column, value=cell.value)
+                    new_cell.font = cell.font
+                    new_cell.border = cell.border
+                    new_cell.fill = cell.fill
+                    new_cell.number_format = cell.number_format
+                    new_cell.alignment = cell.alignment
+                    new_cell.protection = cell.protection
+
+            # Adjust row height to fit content
+            for row in new_sheet.iter_rows():
+                for cell in row:
+                    new_sheet.row_dimensions[cell.row].height = source_sheet.row_dimensions[cell.row].height
+
+            # Align cells to the left
+            for column in new_sheet.columns:
+                for cell in column:
+                    cell.alignment = cell.alignment.copy(horizontal="left")
+
             # Close the source workbook
             source_workbook.close()
         except ValueError:
