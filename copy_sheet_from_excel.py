22083dde0e401c40bfbc74a474c0188994716a29
psbworld6 <133992142+psbworld6@users.noreply.github.com>
2023-06-09 12:33:54 +0530

    Update copy_sheet_from_excel.py

--- copy_sheet_from_excel.py
+++ copy_sheet_from_excel.py
@@ -25,7 +25,7 @@ for filename in os.listdir(directory):
             # Create a new sheet in the consolidated workbook
             new_sheet = consolidated_workbook.create_sheet(title=filename)
 
-            # Copy the sheet with formatting
+            # Copy the sheet with formatting and content
             for row in source_sheet.iter_rows(min_row=1, max_row=source_sheet.max_row, min_col=1, max_col=source_sheet.max_column):
                 for cell in row:
                     new_cell = new_sheet[cell.coordinate]
@@ -66,13 +66,23 @@ for filename in os.listdir(directory):
 
                         new_cell.number_format = cell.number_format
 
+            # Resize the rows to fit the content
+            for row in new_sheet.iter_rows(min_row=1, max_row=new_sheet.max_row):
+                for cell in row:
+                    new_sheet.row_dimensions[cell.row].height = source_sheet.row_dimensions[cell.row].height
+
+            # Align cells to the left
+            for row in new_sheet.iter_rows(min_row=1, max_row=new_sheet.max_row, min_col=1, max_col=new_sheet.max_column):
+                for cell in row:
+                    cell.alignment = Alignment(horizontal="left")
+
             # Rename the new sheet to the file name
             new_sheet.title = filename
 
-print("Copying and renaming sheets completed.")
+print("Copying, formatting, and renaming sheets completed.")
 
 # Save the consolidated workbook to a new file
 consolidated_file_path = os.path.join(directory, "consolidated_reports.xlsx")
 consolidated_workbook.save(consolidated_file_path)
 
-print(f"Consolidated sheets saved to {consolidated_file_path}.")
+print(f"Consolidated sheets saved to {cons
