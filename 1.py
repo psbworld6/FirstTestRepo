a6bede38104e9339b10fa8c50d294e2149521515
psbworld6 <133992142+psbworld6@users.noreply.github.com>
2023-06-22 10:24:40 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,5 @@
 import os
-from openpyxl import load_workbook
-from openpyxl.vba import ProjectModule
+import xlwings as xw
 
 # Path to the directory containing .xlsx files
 directory = '/path/to/directory'
@@ -8,21 +7,25 @@ directory = '/path/to/directory'
 # Path to the .bas file
 bas_file_path = '/path/to/macro.bas'
 
-# Load the .bas file content
-with open(bas_file_path, 'r') as bas_file:
-    bas_code = bas_file.read()
-
 # Iterate over each file in the directory
 for filename in os.listdir(directory):
     if filename.endswith('.xlsx'):
         file_path = os.path.join(directory, filename)
 
-        # Load the workbook
-        workbook = load_workbook(file_path)
+        # Connect to the Excel application
+        app = xw.App(visible=False)
+        workbook = app.books.open(file_path)
 
-        # Create a new VBA module and add the .bas code
-        vba_module = ProjectModule(filename='Module1', content=bas_code)
-        workbook.vba_project.vba_modules.append(vba_module)
+        # Load the .bas file content
+        with open(bas_file_path, 'r') as bas_file:
+            bas_code = bas_file.read()
 
-        # Save the modified workbook
-        workbook.save(file_path)
+        # Run the VBA code
+        workbook.api.Application.Run(bas_code)
+
+        # Save and close the workbook
+        workbook.save()
+        workbook.close()
+
+        # Quit the Excel application
+        app.quit()
